38d6377f162cc5b720da8e4fa4388416c7b2086a
yourwjd <yourwjd13@gmail.com>
2023-11-29 01:14:13 +0900

    main difference

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-    const circle = document.querySelector(".circle");
+//#main 섹션
 
-    document.addEventListener("mousemove", (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    circle.style.left = (mouseX / window.innerWidth) * 100 + "vw";
-    circle.style.top = (mouseY / window.innerHeight) * 100 + "vh";
-    });
+//커서를 따라오는 마우스
+const circle = document.querySelector(".circle");
+
+document.addEventListener("mousemove", (e) => {
+const mouseX = e.clientX;
+const mouseY = e.clientY;
+circle.style.left = (mouseX / window.innerWidth) * 100 + "vw";
+circle.style.top = (mouseY / window.innerHeight) * 100 + "vh";
+});
+
