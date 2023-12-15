f4a9b9febfcf34d0790d4674a7d5d12d9a41897b
yourwjd <yourwjd13@gmail.com>
2023-12-15 10:48:18 +0900

    12/14

--- index.js
+++ index.js
@@ -13,25 +13,26 @@ circle.style.top = (mouseY / window.innerHeight) * 100 + "vh";
 
 //원 커지게
 window.addEventListener('scroll', function() {
+    const main = document.querySelector('#main');
     const zoomCircle = document.querySelector('#main .zoom');
     const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
     const scrollY = (window.scrollY / viewportWidth) * 100; //scrollY vw 단위로 계산
     let scale = 0;
 
     console.log(scrollY);
-    if (scrollY > 0 && scrollY <= 26) {
-        const progress = (scrollY) / 26;
-        scale = 0 + (6- 0) * progress;
-    } else if (scrollY > 26) {
+    if (scrollY > 0 && scrollY <= 45) {
+        const progress = (scrollY) / 45;
+        scale = 0 + (10- 0) * progress;
+        zoomCircle.style.opacity = 1;
+        main.style.position = 'fixed';
+    } else if (scrollY > 45) {
         scale = 10;
+        main.style.position = 'relative';
     }
 
     zoomCircle.style.transform = `translate(-50%, -50%) scale(${scale})`;
 
-    if (scale >= 10) {
-        event.preventDefault();
-      }
-    }, { passive: false });
+});
 
 
 //스크롤 숨기기
@@ -46,7 +47,34 @@ window.addEventListener("scroll", function () {
     }
     else if (currentScrollPosition > 0) { 
         scroll.style.opacity = 0;
-        scroll.style.top = 100 + "%";
+        scroll.style.top = 105 + "%";
     }
 });
 
+const options = {
+    root: null, 
+    rootMargin: '0px', 
+    threshold: 0.1 
+  };
+  
+  const callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) { 
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = 'translateY(0)'; 
+      } else { 
+        entry.target.style.opacity = 0; 
+        entry.target.style.transform = 'translateY(50px)'; 
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback, options);
+  
+  const targets = document.querySelectorAll('.comment, #skill');
+  targets.forEach(target => {
+    target.style.transition = 'opacity 1s, transform 1s'; // 서서히 나타나고 사라지게 함
+    target.style.opacity = 0;
+    target.style.transform = 'translateY(50px)'; // 초기에는 아래로 조금 이동된 상태
+    observer.observe(target); // 각 타겟에 대해 Intersection Observer를 적용함
+  });
